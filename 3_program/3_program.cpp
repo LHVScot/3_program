959866ed05dba14c76d82b33ad2d7c3c7b3c45fa
LHVSkot <lhvskot4@gmiail.com>
2024-09-30 13:54:48 +0900

    Добавьте файлы проекта.

---
+++ 3_program/3_program.cpp
@@ -0,0 +1,53 @@
+﻿#include <iostream>
+#include <math.h>
+using namespace std;
+
+int main()
+{
+    /*
+    variant 14
+    zadamie 14
+    */
+    float f, g, m1, m2;
+    g = 9.8;
+    cout << "F = g*m1*m2";
+    cout << "\nvvedite massy 1: ";
+    cin >> m1;
+    cout << "\nvvedite massy 2: ";
+    cin >> m2;
+    f = g * m1 * m2;
+    cout << "\nF = " << g << "*" << m1 << "*" << m2;
+    cout << "result: " << f;
+
+
+    /*
+    zadanie 11Ж
+
+    float a, z , y, h,b;
+    int x;
+    cout << "\nvvedite z: ";
+    cin >> z;
+    cout << "\nvvedite x: ";
+    cin >> x;
+    cout << "\nvvedite y: ";
+    cin >> y;
+    cout << "\nvvedite h: ";
+    cin >> h;
+    a = h * abs((y - sqrt(abs(x)) * (x - (y / z + (x * x) / 4))));
+    b = x - ((x * x) / 3) + (x ^ 5) / 5;
+    cout << "\nresult a: " << a;
+    cout << "\nresult b: " << b;
+
+
+    zadanie 29
+
+    int x1, y1, c;
+    cout << "\nvvedite y: ";
+    cin >> y;
+    cout << "\nvvedite x: ";
+    cin >> x;
+    c = 3 * x1 ^ 2 * y1 ^ 2 - 2 * x1 * y1 ^ 2 - 7 * x1 ^ 2 * y1 - 4 * y1 ^ 2 + 15 * x1 * y1 + 2 * x1 ^ 2 - 3 * x1 + 10 * y1 + 6;
+    cout << "result c: " << c;
+    return 0;
+    */
+}
